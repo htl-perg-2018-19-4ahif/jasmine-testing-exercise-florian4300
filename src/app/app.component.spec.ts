651bcd645f46b3479bc886720c4e0968e9712caa
Florian Hitzler <20150060@HTBLAPerg.onmicrosoft.com>
2019-05-24 13:13:55 +0200

    added additional test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-import { InvoiceCalculatorService, InvoiceLine, Invoice, InvoiceLineComplete } from './invoice-calculator.service';
-import { FormsModule } from '@angular/forms';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import {
+  InvoiceCalculatorService,
+  InvoiceLine,
+  Invoice,
+  InvoiceLineComplete
+} from "./invoice-calculator.service";
+import { FormsModule } from "@angular/forms";
 
 class InvoiceCalculatorServiceMock {
   public CalculateInvoice(invoiceLines: InvoiceLine[]): Invoice {
@@ -22,47 +27,46 @@ class InvoiceCalculatorServiceMock {
   }
 }
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        FormsModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [FormsModule],
+      declarations: [AppComponent],
       providers: [
-        { provide: InvoiceCalculatorService, useValue: new InvoiceCalculatorServiceMock() }
-      ],
+        {
+          provide: InvoiceCalculatorService,
+          useValue: new InvoiceCalculatorServiceMock()
+        }
+      ]
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should disable add button if description of price are empty', () => {
+  it("should disable add button if description of price are empty", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const de = fixture.debugElement;
     const ne: HTMLElement = de.nativeElement;
     const app: AppComponent = de.componentInstance;
 
-    const addButton: HTMLButtonElement = ne.querySelector('#add-button');
+    const addButton: HTMLButtonElement = ne.querySelector("#add-button");
 
     fixture.detectChanges();
     expect(addButton.disabled).toBeTruthy();
 
-    app.product = 'Pizza';
+    app.product = "Pizza";
     app.priceInclusiveVat = 6;
-    app.vatCategoryString = 'Food';
+    app.vatCategoryString = "Food";
     fixture.detectChanges();
 
     expect(addButton.disabled).toBeFalsy();
   });
 
-  it('should add invoice line correctly', () => {
+  it("should add invoice line correctly", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const de = fixture.debugElement;
     const ne: HTMLElement = de.nativeElement;
@@ -70,50 +74,78 @@ describe('AppComponent', () => {
 
     fixture.detectChanges();
 
-    let invoiceLines = ne.querySelectorAll<HTMLTableRowElement>('#invoice-lines');
+    let invoiceLines = ne.querySelectorAll<HTMLTableRowElement>(
+      "#invoice-lines"
+    );
     expect(invoiceLines.length).toBe(0);
 
-    const productInput: HTMLInputElement = ne.querySelector('#product-input');
-    productInput.value = 'Pizza';
-    productInput.dispatchEvent(new Event('input'));
-    const vatCategoryInput: HTMLSelectElement = ne.querySelector('#vat-category-input');
-    vatCategoryInput.value = 'Food';
-    vatCategoryInput.dispatchEvent(new Event('input'));
-    const priceInclusiveVatInput: HTMLInputElement = ne.querySelector('#priceInclusiveVat-input');
-    priceInclusiveVatInput.value = '6';
-    priceInclusiveVatInput.dispatchEvent(new Event('input'));
+    const productInput: HTMLInputElement = ne.querySelector("#product-input");
+    productInput.value = "Pizza";
+    productInput.dispatchEvent(new Event("input"));
+    const vatCategoryInput: HTMLSelectElement = ne.querySelector(
+      "#vat-category-input"
+    );
+    vatCategoryInput.value = "Food";
+    vatCategoryInput.dispatchEvent(new Event("input"));
+    const priceInclusiveVatInput: HTMLInputElement = ne.querySelector(
+      "#priceInclusiveVat-input"
+    );
+    priceInclusiveVatInput.value = "6";
+    priceInclusiveVatInput.dispatchEvent(new Event("input"));
 
     fixture.detectChanges();
 
-    const addButton: HTMLButtonElement = ne.querySelector('#add-button');
+    const addButton: HTMLButtonElement = ne.querySelector("#add-button");
     addButton.click();
 
     fixture.detectChanges();
 
-    invoiceLines = ne.querySelectorAll<HTMLTableRowElement>('#invoice-lines');
+    invoiceLines = ne.querySelectorAll<HTMLTableRowElement>("#invoice-lines");
     expect(invoiceLines.length).toBe(1);
 
-    expect(invoiceLines[0].children[0].textContent).toBe('Pizza');
-    expect(invoiceLines[0].children[1].textContent).toBe('6.00');
-    expect(invoiceLines[0].children[2].textContent).toBe('42.00');
+    expect(invoiceLines[0].children[0].textContent).toBe("Pizza");
+    expect(invoiceLines[0].children[1].textContent).toBe("6.00");
+    expect(invoiceLines[0].children[2].textContent).toBe("42.00");
   });
 
-  it('should calculate totals correctly', () => {
+  it("should calculate totals correctly", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const de = fixture.debugElement;
     const ne: HTMLElement = de.nativeElement;
     const app: AppComponent = de.componentInstance;
 
-    app.product = 'Pizza';
+    app.product = "Pizza";
     app.priceInclusiveVat = 6;
-    app.vatCategoryString = 'Food';
+    app.vatCategoryString = "Food";
     app.addInvoice();
 
     fixture.detectChanges();
 
-    const totalPriceInclusiveVatInput: HTMLTableDataCellElement = ne.querySelector('#totalPriceInclusiveVat');
-    expect(totalPriceInclusiveVatInput.textContent).toBe('84.00');
-    const totalPriceExclusiveVatInput: HTMLTableDataCellElement = ne.querySelector('#totalPriceExclusiveVat');
-    expect(totalPriceExclusiveVatInput.textContent).toBe('42.00');
+    const totalPriceInclusiveVatInput: HTMLTableDataCellElement = ne.querySelector(
+      "#totalPriceInclusiveVat"
+    );
+    expect(totalPriceInclusiveVatInput.textContent).toBe("84.00");
+    const totalPriceExclusiveVatInput: HTMLTableDataCellElement = ne.querySelector(
+      "#totalPriceExclusiveVat"
+    );
+    expect(totalPriceExclusiveVatInput.textContent).toBe("42.00");
+  });
+  it("should disable button when price is negative or 0", () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const de = fixture.debugElement;
+    const ne: HTMLElement = de.nativeElement;
+    const app: AppComponent = de.componentInstance;
+
+    const addButton: HTMLButtonElement = ne.querySelector("#add-button");
+
+    fixture.detectChanges();
+    expect(addButton.disabled).toBeTruthy();
+
+    app.product = "Pizza";
+    app.priceInclusiveVat = -1;
+    app.vatCategoryString = "Food";
+    fixture.detectChanges();
+
+    expect(addButton.disabled).toBeFalsy();
   });
 });
